dde17387e5682545bf552896dfbd21d443e0afb3
bootandy <bootandy@gmail.com>
2018-02-07 10:17:58 -0500

    Reduce to minimum for crash
    Context not needed
    
    to fix error either don't mount the fairing in main or remove one of the
    'ifs' in the template
    
    Error seen:
    ...
      Mounting '/':
        => GET /
    
    thread 'main' has overflowed its stack
    fatal runtime error: stack overflow
    Abort trap: 6

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,12 @@ extern crate rocket_contrib;
 extern crate serde_derive;
 extern crate tera;
 
-use std::cmp;
-use std::path::{Path, PathBuf};
-
 use rocket_contrib::Template;
-use rocket::http::Status;
-use rocket::outcome::Outcome::*;
-use rocket::request::{Form, FromRequest, Outcome, Request};
-use rocket::response::NamedFile;
-use tera::Context;
 
-#[derive(Serialize)]
-struct TemplateContext {
-        keyword: String,
-        keyword_help_link: String,
-        sql_correct: String,
-        sql_to_run: String,
-        sql_correct_result: Vec<Vec<String>>,
-        sql_to_run_result: Vec<Vec<String>>,
-        next_q: String,
-        prev_q: String,
-        is_correct: bool,
-        used_correct_word: bool,
-}
 
-#[get("/questions/<_question>")]
-fn get_db(_question: String) -> Template {
-    let c = TemplateContext {
-        keyword: "t.keywords[0]".to_string(),
-        keyword_help_link: "t.help_link".to_string(),
-        sql_correct: "t.sql".to_string(),
-        sql_to_run : "asdf".to_string(),
-        sql_correct_result: vec![vec!["asd".to_string()]],
-        sql_to_run_result: vec![vec!["sql_result".to_string()]],
-        next_q: "asdf".to_string(),
-        prev_q: "3".to_string(),
-        is_correct: true,
-        used_correct_word: false,
-    };
-    Template::render(_question, &c)
+#[get("/")]
+fn get_db() -> Template {
+    Template::render("all_questions", ())
 }
 
 fn rocket() -> rocket::Rocket {
